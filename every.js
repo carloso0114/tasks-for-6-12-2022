a9e38014e17df89f6de9f177f4186362c8f6555a
carloso0114 <carlossospinag@gmail.com>
2022-12-06 01:24:37 -0500

    add semicolons

--- every.js
+++ every.js
@@ -6,8 +6,8 @@ const every2 = (arr, everyFunc) => {
   for (let i = 0; i < arr.length; i++) {
     if (!everyFunc(arr[i], i, arr)) {
       return false;
-    }
-  }
+    };
+  };
   return true;
 }
 console.log(every2(numberArray, (num, index, arr) => {
